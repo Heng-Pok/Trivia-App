abe7a303eecf0f39cc687f4749ce10cdfa0459ec
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-10 16:55:26 -0700

    trivia_data

---
+++ src/TriviaData.js
@@ -0,0 +1,116 @@
+const triviaData = {
+    animal: {
+        subcategories: {
+            mammals: [
+                { question: "What is the largest land mammal?", options: ["Elephant", "Whale", "Giraffe", "Lion"], correct: "Elephant", points: 100 },
+                { question: "What mammal lays eggs?", options: ["Platypus", "Kangaroo", "Tiger", "Seal"], correct: "Platypus", points: 200 },
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            birds: [
+                { question: "Which bird cannot fly?", options: ["Penguin", "Eagle", "Owl", "Sparrow"], correct: "Penguin", points: 100 },
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            fish: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            reptiles: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ]
+        },
+    },
+    plants: {
+        subcategories: {
+            flowers: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            trees: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            edible: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            poisonous: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ]
+        }
+    },
+    culture: {
+        subcategories: {
+            language: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            Art: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            Literature: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            PopCulture: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ]
+        }
+    },
+    history: {
+        subcategories: {
+            discoveries: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            HistoricalFigures: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            HistoricalEras: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ],
+            WorldHistory: [
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 100},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 200},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 300},
+                {question: "q", options: ["option 1", "option 2", "option 3", "option 4"], correct: "option 1", points: 400},
+            ]
+        }
+    }
+};
+
+export default triviaData;

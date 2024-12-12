5a460747f342e2a8e9e3d31a131c9266e76a3dc7
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-12 15:42:30 -0700

    App.jsx passes user info to GameScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import LoginForm from "./LoginForm.jsx"; // Ensure this path is correct
 import MainPage from "./MainPage.jsx";
-import Dummy from "./Dummy.jsx"
 import SignupForm from "./SignupForm.jsx";
 import GameScreen from "./GameScreen.jsx";
+import {useState} from "react";
 
 function App() {
+
+    const [currentUser, setCurrentUser] = useState(null);
+
   return (
     <Routes>
       <Route path="/" element={<MainPage/>}/>
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/signup" element={<SignupForm />} />
-      <Route path="/gamescreen" element={<GameScreen/>}/>
+      <Route path="/login" element={<LoginForm setUser={setCurrentUser} />} />
+      <Route path="/signup" element={<SignupForm setUser={setCurrentUser} />} />
+      <Route path="/gamescreen" element={<GameScreen user={currentUser} />}/>
     </Routes>
   );
 }

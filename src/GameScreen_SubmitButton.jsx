a73b5239f960709db6347c016f0a3a4e8671258d
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-11 19:29:20 -0700

    added submit button on gamescreen

---
+++ src/GameScreen_SubmitButton.jsx
@@ -0,0 +1,10 @@
+
+// eslint-disable-next-line react/prop-types
+const GameScreen_SubmitButton = ({onSubmitClick}) => {
+
+    return (
+        <button onClick={onSubmitClick}>Submit Game</button>
+    )
+}
+
+export default GameScreen_SubmitButton;

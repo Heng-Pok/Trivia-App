3d42fff38ecd36f15db799c10f16ab1b5a088502
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-13 06:43:56 -0700

    randomizes questions

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,11 +1,34 @@
 // eslint-disable-next-line no-unused-vars
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Board = ({ categoryData, onQuestionClick, disabledButtons }) => {
-
     // eslint-disable-next-line react/prop-types
     const subcategories = Object.keys(categoryData.subcategories);
+
+    const [randomizedQuestions, setRandomizedQuestions] = useState([]);
+
+    // Function to randomize questions
+    const getRandomQuestions = () => {
+        const newRandomizedQuestions = [];
+
+        subcategories.forEach((sub) => {
+            const questions = categoryData.subcategories[sub];
+
+            // Shuffle the questions in the subcategory to pick 4 random ones for the score levels
+            const shuffledQuestions = [...questions].sort(() => 0.5 - Math.random());
+
+            // Add the shuffled questions to the new array, picking the top 4
+            newRandomizedQuestions.push(shuffledQuestions.slice(0, 4));
+        });
+
+        setRandomizedQuestions(newRandomizedQuestions);
+    };
+
+    // Run the randomization when the component mounts
+    useEffect(() => {
+        getRandomQuestions();
+    }, [categoryData]);
 
     return (
         <div className="board">
@@ -19,11 +42,10 @@ const Board = ({ categoryData, onQuestionClick, disabledButtons }) => {
             </div>
 
             {/* Question Buttons */}
-            {Array.from({ length: 5 }).map((_, rowIndex) => (
+            {Array.from({ length: 4 }).map((_, rowIndex) => (
                 <div key={rowIndex} className="row">
                     {subcategories.map((sub, colIndex) => {
-                        // eslint-disable-next-line react/prop-types
-                        const question = categoryData.subcategories[sub][rowIndex];
+                        const question = randomizedQuestions[colIndex] ? randomizedQuestions[colIndex][rowIndex] : null;
                         return question ? (
                             <button
                                 key={colIndex}
@@ -31,7 +53,6 @@ const Board = ({ categoryData, onQuestionClick, disabledButtons }) => {
                                 disabled={disabledButtons[`${rowIndex}-${colIndex}`]}
                                 className="question-btn"
                             >
-                                {/* eslint-disable-next-line react/prop-types */}
                                 {question.points}
                             </button>
                         ) : (

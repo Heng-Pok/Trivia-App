52946daca32f7cc56ba213b21dad22c8a7566836
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-13 04:32:08 -0700

    added logout mechanics

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -2,10 +2,11 @@ import Header from "./Header.jsx"
 import Body from "./Body.jsx"
 import Footer from "./Footer.jsx"
 
-function MainPage (){
+// eslint-disable-next-line react/prop-types
+function MainPage ( {setUser} ){
     return (
         <>
-        <Header/>
+        <Header setUser={setUser}/>
         <Body/>
         <Footer/>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quis laboriosam aspernatur cum, illo quod nisi nobis officia reprehenderit libero officiis nemo numquam autem nesciunt dicta provident omnis tempore voluptatum.</p>

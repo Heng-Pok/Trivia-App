a4826a1d081a5fb797830c534415240e7cca758c
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-13 15:55:24 -0700

    header has access to who is signed in

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,12 +3,13 @@ import Body from "./Body.jsx"
 import Footer from "./Footer.jsx"
 
 // eslint-disable-next-line react/prop-types
-function MainPage ( {setUser} ){
+function MainPage ( {user, setUser} ){
     return (
         <>
-        <Header setUser={setUser}/>
+        <Header setUser={setUser} user={user}/>
         <Body/>
         <Footer/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quis laboriosam aspernatur cum, illo quod nisi nobis officia reprehenderit libero officiis nemo numquam autem nesciunt dicta provident omnis tempore voluptatum.</p>
         </>
     )
 }

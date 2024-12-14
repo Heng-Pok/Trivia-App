ef9173437acbb809542536ed8e59e6b52d9c592e
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-13 22:31:53 -0700

    Update PlantCard.jsx

--- src/PlantCard.jsx
+++ src/PlantCard.jsx
@@ -5,7 +5,7 @@ function PlantCard( {onClick} ){
     return (
         <div className="card" onClick={onClick}>
             <img src = {profilePic} alt = "plants-picture" />
-            <h2>Topic: Plants</h2>
+            <h2>Topic: Plants Trivia</h2>
             <p><b>Description:</b> Plant related questions concerning flowers, trees, edible plants, and plant anatomy.</p>
         </div>
     )

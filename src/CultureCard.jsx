8e50dba7a169a5881097994ccc7166f9c1d7ff0f
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-13 20:20:13 -0700

    Update CultureCard.jsx

--- src/CultureCard.jsx
+++ src/CultureCard.jsx
@@ -1,13 +1,13 @@
 import profilePic from "./assets/culture.jpg"
 
+// eslint-disable-next-line react/prop-types
 function CultureCard( {onClick} ){
 
     return (
         <div className="card" onClick={onClick}> 
             <img src = {profilePic} alt = "cultures-picture" />
-            <h2>Topic: Cultural Studies</h2>
-            <p><b>Difficulty:</b> Medium</p>
-            <p><b>Description:</b> Comparing and contrasting different cultures.</p>
+            <h2>Topic: Cultural Trivia</h2>
+            <p><b>Description:</b> Culture related questions concerning language, art, literature, and pop culture.</p>
         </div>
     )
 }

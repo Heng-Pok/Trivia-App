455cd656233b67678a623e7adb8b91ef6fd1eb54
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-13 20:20:41 -0700

    Update HistoryCard.jsx

--- src/HistoryCard.jsx
+++ src/HistoryCard.jsx
@@ -1,13 +1,13 @@
 import profilePic from "./assets/American-Revolution(3).jpg"
 
+// eslint-disable-next-line react/prop-types
 function HistoryCard( {onClick} ){
 
     return (
         <div className="card" onClick={onClick}>
             <img src={profilePic} alt="history-picture" />
-            <h2>Topic: World History</h2>
-            <p><b>Difficulty:</b> Hard</p>
-            <p><b>Description:</b> From the beginning of the Roman Empire to the Cold War</p>
+            <h2>Topic: History Trivia</h2>
+            <p><b>Description:</b> History related questions concerning historical discoveries, historical figures, historical eras, and world history</p>
         </div>
     )
 }
